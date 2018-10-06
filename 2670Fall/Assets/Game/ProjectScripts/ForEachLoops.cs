83b3e9da0de26d7ccd5ddce339795c2490841dc0
Anthony Romrell <anthonyromrell@mac.com>
2018-10-06 12:36:46 -0600

    structural changes

--- 2670Fall/Assets/Game/ProjectScripts/ForEachLoops.cs
+++ 2670Fall/Assets/Game/ProjectScripts/ForEachLoops.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ArtisanDream.Tools.Objects;
 using UnityEngine;
 
 //Made By Anthony Romrell
--- 2670Fall/Assets/ObjectID.cs
+++ 2670Fall/Assets/ObjectID.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ArtisanDream.Experimental.Objects;
 using UnityEngine;
+
 
 public class ObjectID : MonoBehaviour
 {
